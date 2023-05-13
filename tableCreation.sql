c53af196561436c77022a14a8b072e518924a73f
Brahmpreet Singh <bsingh2@unb.ca>
2023-05-13 19:06:03 -0300

    setting up the users and drivers table in the database

---
+++ tableCreation.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (userId INT PRIMARY KEY, phoneNum varchar(10), userName varchar(20));
+
+DROP TABLE IF EXISTS drivers;
+CREATE TABLE drivers (driverId INT PRIMARY KEY, driverName varchar(20));
+
