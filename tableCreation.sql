91f1b666b221dc0bd2aaa8df25770635cb41c113
Adnan Khan <ahmadadnankhank1@gmail.com>
2023-05-14 01:18:03 +0300

    added bookings table in the database

--- tableCreation.sql
+++ tableCreation.sql
@@ -1,6 +1,24 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (userId INT PRIMARY KEY, phoneNum varchar(10), userName varchar(20));
+CREATE TABLE users (
+    userId INT PRIMARY KEY, 
+    phoneNum varchar(10), 
+    userName varchar(20)
+);
 
 DROP TABLE IF EXISTS drivers;
-CREATE TABLE drivers (driverId INT PRIMARY KEY, driverName varchar(20));
+CREATE TABLE drivers (
+    driverId INT PRIMARY KEY, 
+    driverName varchar(20)
+);
 
+DROP TABLE IF EXISTS booking;
+CREATE TABLE booking (
+    bookingId INT PRIMARY KEY,
+    timeBooked varchar(8), -- YYYYMMDD
+    pickup text, --Longitude and Latitude
+    dropoff text, --Longitude and Latitude
+    userId INT,
+    driverID INT,
+    FOREIGN KEY (userId) REFERENCES users(userId),
+    FOREIGN KEY (driverID) REFERENCES drivers(driverID)
+);
