87628266650b0082ac160091933ff9461ddafb44
Brahmpreet Singh <bsingh2@unb.ca>
2023-05-27 21:50:22 -0300

    added post api call for signup of user

--- src/app.py
+++ src/app.py
@@ -5,11 +5,11 @@ cur = conn.cursor()
 
 # f = open("tableCreation.sql")
 
-cur.execute('INSERT into users (phoneNum, userName) Values ("5063215543", "aakhan");')
 # cur.execute('INSERT into users (phoneNum, userName) Values ("5063215543", "aakhan");')
-cur.execute('INSERT into users (phoneNum, userName) Values ("4567892341", "brahmpreetS");')
-cur.execute('INSERT into users (phoneNum, userName) Values ("3421563452", "danielA");')
-conn.commit()
+# # cur.execute('INSERT into users (phoneNum, userName) Values ("5063215543", "aakhan");')
+# cur.execute('INSERT into users (phoneNum, userName) Values ("4567892341", "brahmpreetS");')
+# cur.execute('INSERT into users (phoneNum, userName) Values ("3421563452", "danielA");')
+# conn.commit()
 cur.execute('SELECT * from users')
 
 print(cur.fetchall())
--- src/flaskApp.py
+++ src/flaskApp.py
@@ -19,5 +19,33 @@ class signIn(Resource):
 api.add_resource(signIn, '/login')
 
 
+class signUp(Resource):
+    def post(self):
+        if not request.json:
+            abort(400)
+        
+        #Parse the json
+        parser = reqparse.RequestParser()
+        try:
+            # Check for required attributes in json document, create a dictionary
+            parser.add_argument('userName', type=str, required=True)
+            parser.add_argument('phoneNum', type=str, required=True)
+            request_params = parser.parse_args()
+        except:
+            abort(400)
+        
+        try: 
+            conn = sqlite3.connect('taxi.db')
+            cur = conn.cursor()
+            cur.execute(f"INSERT into users (phoneNum, userName) Values (\"{request_params['phoneNum']}\", \"{request_params['userName']}\");")
+            # cur.execute(f"INSERT into users (phoneNum, userName) Values ("hello", "506444333");")
+
+            conn.commit()
+        except:
+            abort(500)
+        return make_response(jsonify("added"), 201)
+
+api.add_resource(signUp, '/v1/signUp')
+
 if __name__ == "__main__":
     app.run(debug=True)
